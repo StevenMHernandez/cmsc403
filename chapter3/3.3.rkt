f0361804304f9291083cbb6e7f41e37d2bb2d28b
Steven Hernandez <contact@stevenHdesigns.com>
2017-10-02 16:03:47 -0400

    3.3 fix order of output list by using `append` instead of `cons`

--- chapter3/3.3.rkt
+++ chapter3/3.3.rkt
@@ -1,18 +1,14 @@
 #lang racket
-(define increase-salary-r (lambda (percentage Salaries SalariesSoFar)
-                            (if (null? Salaries)
-                                SalariesSoFar
-                                (increase-salary-r percentage (cdr Salaries) (cons
-                                                                              (* (+ 1 percentage)
-                                                                                 (car Salaries)
-                                                                                 )
-                                                                              SalariesSoFar)
-                                                   ))
-                            )
+
+(define increase-salary (lambda (percentage Salaries SalariesSoFar)
+                          (if (null? Salaries)
+                              SalariesSoFar
+                              (increase-salary percentage (cdr Salaries)
+                                               (append SalariesSoFar
+                                                       (list (* (+ 1 percentage) (car Salaries))))
+                                               )
+                              )
+                          )
   )
 
-(define increase-salary (lambda (percentage Salaries)
-                          (increase-salary-r percentage Salaries '())
-                          ))
-
-(increase-salary 0.03 '(100 200 1000)) ; '(103 206 1030)
+(increase-salary 0.03 '(100 200 1000) '()) ; '(103 206 1030)
